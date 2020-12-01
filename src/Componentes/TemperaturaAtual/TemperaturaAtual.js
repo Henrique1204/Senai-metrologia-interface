7666a41fc461a7123a32abc0784e97fa3d5f1a4c
Paulo <pauloh16.pdr@gmail.com>
2020-12-01 20:24:42 -0300

    Criar componente CardTemperatura

--- src/Componentes/TemperaturaAtual/TemperaturaAtual.js
+++ src/Componentes/TemperaturaAtual/TemperaturaAtual.js
@@ -1,9 +1,17 @@
 import React from "react";
+import CardTemperatura from "./CardTemperatura/CardTemperatura";
 
 const TemperaturaAtual = () => {
+    const dados = {
+        temperatura: 28,
+        umidade: 10,
+        nome_sensor: "A"
+    }
+
     return (
         <section className="container">
             <h1 className="titulo">Temperaturas atuais</h1>
+            <CardTemperatura dados={dados} />
         </section>
     );
 };
