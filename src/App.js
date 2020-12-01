3d5140ea00ba092ccd4e5634c99940c603353ba2
RachMor <raquel.costt@gmail.com>
2020-11-30 22:15:08 -0300

    Criar rotas principais do projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+// Import dos componentes do React Router.
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-function App() {
-  return <div></div>;
+
+const App = () => {
+  return (
+    /*BrowserRouter é necessário para as rotas funcionarem.*/
+    <BrowserRouter>
+    {/* Routes é o componente que cria as rotas. */}
+    <Routes>
+      {/* Router é a rota. */}
+      {/* "path" é a url, "element" é o componente que irá renderizar. */}
+      <Route path="/" element={<div>Home</div>}/>
+      <Route path="/sensorA" element={<div>Sensor A</div>}/>
+      <Route path="/sensorB" element={<div>Sensor B</div>}/>
+      <Route path="/sensorC" element={<div>Sensor C</div>}/>
+      <Route path="/sensorD" element={<div>Sensor D</div>}/>
+      <Route path="/sensorE" element={<div>Sensor E</div>}/>
+    </Routes>
+    
+    </BrowserRouter>
+
+  );
 }
 
 export default App;
