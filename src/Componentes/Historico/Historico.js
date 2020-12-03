f3a2f1b620938801d644de748a4857e49f1f0495
Julia <juagathasantos@gmail.com>
2020-12-03 17:00:36 -0300

    Estilizar componente Historico

--- src/Componentes/Historico/Historico.js
+++ src/Componentes/Historico/Historico.js
@@ -1,18 +1,19 @@
 import React from "react";
-
+import estilos from "./Historico.module.css";
 const Historico = () => {
     return (
         <section className="container">
-            <table>
-                <tr>
+            <table className={estilos.tabela}>
+
+                <tr className={estilos.titulos}>
                     <th>Nome Sensor</th>
                     <th>Temperatura</th>
                     <th>Umidade</th>
                     <th>Data</th>
                     <th>Hora</th>
                 </tr>
-                
-                <tr>
+
+                <tr className={estilos.dados}>
                     <td>A</td>
                     <td>28 ºC</td>
                     <td>12</td>
