036ebf8007f2b6980c7be57f6a5d4f12609c3bce
Paulo <pauloh16.pdr@gmail.com>
2020-12-03 09:00:33 -0300

    Criar geracao de paginas no slide

--- src/Componentes/Slide/Slide/Slide.js
+++ src/Componentes/Slide/Slide/Slide.js
@@ -6,18 +6,44 @@ import BotaoSlide from "../BotaoSlide/BotaoSlide.js";
 import estilos from "./Slide.module.css";
 
 const Slide = ({ dados }) => {
+    const [graficos, setGraficos] = React.useState([]);
     const [titulo, setTitulo] = React.useState("Sensor A");
+
+    React.useEffect(() => {
+        function separarDados(sensor) {
+            return dados.filter((dado) => dado.nome_sensor === sensor);
+        }
+    
+        setGraficos([
+            separarDados("A"),
+            separarDados("B"),
+            separarDados("C"),
+            separarDados("D"),
+            separarDados("E"),
+        ]);
+
+    }, [dados]);
 
     return (
         <section className={estilos.container}>
             <div className="container">
                 <h1 className="titulo">{titulo}</h1>
 
-                <Grafico dados={dados} />
-
-                <div>
-                    <BotaoSlide valor={0} ativo={true} />
+                <div className={estilos.slide}>
+                    {
+                        graficos && graficos.map((dados, i) => (
+                            <Grafico key={i} dados={dados} />
+                        ))
+                    }
                 </div>
+
+                <nav>
+                    {
+                        graficos && graficos.map((d, i) => (
+                            <BotaoSlide valor={i} ativo={true} />
+                        ))
+                    }
+                </nav>
             </div>
         </section>
     );
