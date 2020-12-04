dbd4cffbd2ff8c101c389d3b311c6c98536b6da8
Paulo <pauloh16.pdr@gmail.com>
2020-12-04 18:54:05 -0300

    Adicionar responsividade na página home

--- src/Componentes/TemperaturaAtual/TemperaturaAtual.js
+++ src/Componentes/TemperaturaAtual/TemperaturaAtual.js
@@ -1,13 +1,15 @@
 import React from "react";
 // Import componente CardTemperatura.
 import CardTemperatura from "./CardTemperatura/CardTemperatura";
+// Import do css como módulo.
+import estilos from "./TemperaturaAtual.module.css";
 
 const TemperaturaAtual = ({ dados }) => {
     return (
         <section className="container animarEntrada">
             <h1 className="titulo">Temperaturas atuais</h1>
 
-            <ul style={{ display: "flex", alignItems: "center" }}>
+            <ul className={estilos.container} >
                 {
                     dados && dados.filter((dado, i) => i < 5).map((dado, i) => (
                         <li key={i} style={(i !== 0) ? { marginLeft: "2rem" } : {}} >
