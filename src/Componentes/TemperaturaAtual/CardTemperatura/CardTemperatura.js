abcddb98ae98800702485d74974cabdb34782eae
Paulo <pauloh16.pdr@gmail.com>
2021-02-25 16:26:08 -0300

    Corrigir estilos

--- src/Componentes/TemperaturaAtual/CardTemperatura/CardTemperatura.js
+++ src/Componentes/TemperaturaAtual/CardTemperatura/CardTemperatura.js
@@ -4,18 +4,9 @@ import estilos from "./CardTemperatura.module.css";
 
 const CardTemperatura = ({ dados }) => {
     const [corAviso, setCorAviso] = React.useState(null);
-    const [ativo, setAtivo] = React.useState(true);
 
     React.useEffect(() => {
-        const { temperatura, data, hora } = dados;
-
-        const dataAtual = new Date();
-        const dataCard = Number(data.substring(0, 2));
-        const horaCard = Number(hora.split("h")[1].replace("m", ""));
-
-        const condicaoData = dataCard === dataAtual.getDate();
-        const condicaoHora = horaCard > dataAtual.getMinutes() - 15;
-        setAtivo(condicaoData && condicaoHora);
+        const { temperatura } = dados;
 
         if (temperatura >= 18 && temperatura <= 22) {
             setCorAviso("verde");
@@ -30,7 +21,7 @@ const CardTemperatura = ({ dados }) => {
 
     if (dados) {
         return (
-            <div className={`${estilos.card} ${estilos[corAviso]} ${(!ativo) ? estilos.desativado : '' }`}>
+            <div className={`${estilos.card} ${estilos[corAviso]}`}>
                 <p className={estilos.nome} >Sensor {dados.nome_sensor}</p>
                 <span className={estilos.temperatura} >
                     T • {dados.temperatura.toString().replace(".", ",")} º C
